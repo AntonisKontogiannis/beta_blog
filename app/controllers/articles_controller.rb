1ba181250ccd7f8063a0855ae0bc3d342be76406
Antonis Kontogiannis <antonios.kontogiannis94@gmail.com>
2021-06-18 17:37:46 +0300

    Session and user restrictions added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   # GET /articles or /articles.json
   def index
@@ -22,6 +24,7 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
+    @article.user = current_user
       if @article.save
         flash[:notice] = "Article was successfully created!"
         redirect_to @article
@@ -32,6 +35,7 @@ class ArticlesController < ApplicationController
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
+    @article.user = current_user
     respond_to do |format|
       if @article.update(article_params)
         format.html { redirect_to @article, notice: "Article was successfully updated." }
@@ -62,4 +66,11 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
+    
+    def require_same_user
+      if current_user != @article.user
+          flash[:alert] = "You don't have the authority to do this!"
+          redirect_to @article
+      end
+  end
 end
